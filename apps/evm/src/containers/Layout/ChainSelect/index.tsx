ed8b394ad79467869271c86685ceb0eac7dd99b8
Jawad Ahmed <ja1389180@gmail.com>
2024-04-08 11:11:57 +0500

    feat: update tokens

--- apps/evm/src/containers/Layout/ChainSelect/index.tsx
+++ apps/evm/src/containers/Layout/ChainSelect/index.tsx
@@ -1,27 +1,31 @@
-import { Select, type SelectOption } from 'components';
-import { CHAIN_METADATA } from 'constants/chainMetadata';
-import { useTranslation } from 'libs/translations';
-import { chains, useChainId, useSwitchChain } from 'libs/wallet';
-import type { ChainId } from 'types';
-import { cn } from 'utilities';
+import { Select, type SelectOption } from "components";
+import { CHAIN_METADATA } from "constants/chainMetadata";
+import { useTranslation } from "libs/translations";
+import { chains, useChainId, useSwitchChain } from "libs/wallet";
+import type { ChainId } from "types";
+import { cn } from "utilities";
 
 export interface ChainSelectProps {
   className?: string;
   buttonClassName?: string;
 }
 
-const options: SelectOption<ChainId>[] = chains.map(chain => {
+const options: SelectOption<ChainId>[] = chains.map((chain) => {
   const metadata = CHAIN_METADATA[chain.id as ChainId];
 
   return {
     label: ({ isRenderedInButton }) => (
       <div className="flex items-center">
-        <img src={metadata.logoSrc} alt={metadata.name} className="w-5 max-w-none flex-none" />
+        <img
+          src={metadata.logoSrc}
+          alt={metadata.name}
+          className="w-5 max-w-none flex-none"
+        />
 
         <span
           className={cn(
-            'ml-2 grow overflow-hidden text-ellipsis',
-            isRenderedInButton && 'hidden lg:block',
+            "ml-2 grow overflow-hidden text-ellipsis",
+            isRenderedInButton && "hidden lg:block"
           )}
         >
           {metadata.name}
@@ -32,7 +36,10 @@ const options: SelectOption<ChainId>[] = chains.map(chain => {
   };
 });
 
-export const ChainSelect: React.FC<ChainSelectProps> = ({ className, buttonClassName }) => {
+export const ChainSelect: React.FC<ChainSelectProps> = ({
+  className,
+  buttonClassName,
+}) => {
   const { t } = useTranslation();
   const { chainId } = useChainId();
   const { switchChain } = useSwitchChain();
@@ -40,12 +47,12 @@ export const ChainSelect: React.FC<ChainSelectProps> = ({ className, buttonClass
   return (
     <Select
       value={chainId}
-      onChange={newChainId => switchChain({ chainId: Number(newChainId) })}
+      onChange={(newChainId) => switchChain({ chainId: Number(newChainId) })}
       options={options}
       menuPosition="right"
-      menuTitle={t('layout.chainSelect.label')}
+      menuTitle={t("layout.chainSelect.label")}
       buttonClassName={buttonClassName}
-      className={cn('lg:min-w-[200px]', className)}
+      className={cn("lg:min-w-[200px]", className)}
     />
   );
 };
